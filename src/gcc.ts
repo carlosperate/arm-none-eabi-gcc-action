a379849894bd50334661e6ad8e03905a334153d5
gretel <code@jitter.eu>
2020-06-10 02:34:53 +0200

    add '9-2020-q2-update'

--- src/gcc.ts
+++ src/gcc.ts
@@ -1,4 +1,6 @@
 const versions: {[key: string]: string} = {
+  '9-2020-q2-update':
+    'https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-${ARCH_OS}.${EXT}',
   '9-2019-q4':
     'https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/gcc-arm-none-eabi-9-2019-q4-major-${ARCH_OS}.${EXT}',
   '8-2019-q3':
