0d9e5105a3a1d70cf6e77c342510b02e70b2b154
carlosperate <carlosperate@users.noreply.github.com>
2021-10-29 11:35:14 +0100

    Action: Add zip MD5 to cached directory to validate at load stage.

--- dist/index.js
+++ dist/index.js
@@ -1662,18 +1662,31 @@ function install(release, platform) {
         const distData = gcc.distributionUrl(release, platform);
         const cachedDirectory = tc.find(toolName, release, platform);
         if (cachedDirectory) {
-            core.info(`Cached version found and loaded: ${cachedDirectory}`);
-            return cachedDirectory;
-        }
-        core.info(`Downloading GCC ${release} from ${distData.url} ; MD5 ${distData.md5}`);
-        const gccDownloadPath = yield tc.downloadTool(distData.url);
-        if (distData.md5) {
-            core.info(`GCC release downloaded, calculating MD5...`);
-            const downloadHash = yield md5_file_1.default(gccDownloadPath);
-            core.info(`Downloaded file MD5: ${downloadHash}`);
-            if (downloadHash !== distData.md5) {
-                throw new Error(`Downloaded GCC MD5 doesn't match expected value: ${downloadHash} != ${distData.md5}`);
+            core.info(`Cached version found: ${cachedDirectory}`);
+            let cacheMd5 = 'MD5 not found in cached installation';
+            try {
+                cacheMd5 = yield fs.promises.readFile(path.join(cachedDirectory, 'md5.txt'), { encoding: 'utf8' });
             }
+            catch (err) {
+                core.warning('Could not read the contents of the cached version MD5.');
+                core.warning(err.message);
+            }
+            core.info(`Cached version MD5: ${cacheMd5}`);
+            if (cacheMd5 !== distData.md5) {
+                core.info(`Cached version MD5 does not match: ${cacheMd5} != ${distData.md5}`);
+            }
+            else {
+                core.info('Cached version loaded.');
+                return cachedDirectory;
+            }
+        }
+        core.info(`Cache miss, downloading GCC ${release} from ${distData.url} ; MD5 ${distData.md5}`);
+        const gccDownloadPath = yield tc.downloadTool(distData.url);
+        core.info(`GCC release downloaded, calculating MD5...`);
+        const downloadHash = yield md5_file_1.default(gccDownloadPath);
+        core.info(`Downloaded file MD5: ${downloadHash}`);
+        if (distData.md5 && downloadHash !== distData.md5) {
+            throw new Error(`Downloaded GCC MD5 doesn't match expected value: ${downloadHash} != ${distData.md5}`);
         }
         core.info(`Extracting ${gccDownloadPath}`);
         let extractedPath = '';
@@ -1686,6 +1699,8 @@ function install(release, platform) {
         else {
             throw new Error(`Can't decompress ${distData.url}`);
         }
+        core.info(`Adding to cache: ${extractedPath}`);
+        yield fs.promises.writeFile(path.join(extractedPath, 'md5.txt'), downloadHash, { encoding: 'utf8' });
         const cachedPath = yield tc.cacheDir(extractedPath, toolName, release, platform);
         core.addPath(cachedPath);
         return extractedPath;
--- src/setup.ts
+++ src/setup.ts
@@ -14,20 +14,31 @@ export async function install(release: string, platform?: string): Promise<strin
 
   const cachedDirectory = tc.find(toolName, release, platform);
   if (cachedDirectory) {
-    core.info(`Cached version found and loaded: ${cachedDirectory}`);
-    return cachedDirectory;
+    core.info(`Cached version found: ${cachedDirectory}`);
+    let cacheMd5 = 'MD5 not found in cached installation';
+    try {
+      cacheMd5 = await fs.promises.readFile(path.join(cachedDirectory, 'md5.txt'), {encoding: 'utf8'});
+    } catch (err) {
+      core.warning('Could not read the contents of the cached version MD5.');
+      core.warning(err.message);
+    }
+    core.info(`Cached version MD5: ${cacheMd5}`);
+    if (cacheMd5 !== distData.md5) {
+      core.info(`Cached version MD5 does not match: ${cacheMd5} != ${distData.md5}`);
+    } else {
+      core.info('Cached version loaded.');
+      return cachedDirectory;
+    }
   }
 
-  core.info(`Downloading GCC ${release} from ${distData.url} ; MD5 ${distData.md5}`);
+  core.info(`Cache miss, downloading GCC ${release} from ${distData.url} ; MD5 ${distData.md5}`);
   const gccDownloadPath = await tc.downloadTool(distData.url);
 
-  if (distData.md5) {
-    core.info(`GCC release downloaded, calculating MD5...`);
-    const downloadHash = await md5File(gccDownloadPath);
-    core.info(`Downloaded file MD5: ${downloadHash}`);
-    if (downloadHash !== distData.md5) {
-      throw new Error(`Downloaded GCC MD5 doesn't match expected value: ${downloadHash} != ${distData.md5}`);
-    }
+  core.info(`GCC release downloaded, calculating MD5...`);
+  const downloadHash = await md5File(gccDownloadPath);
+  core.info(`Downloaded file MD5: ${downloadHash}`);
+  if (distData.md5 && downloadHash !== distData.md5) {
+    throw new Error(`Downloaded GCC MD5 doesn't match expected value: ${downloadHash} != ${distData.md5}`);
   }
 
   core.info(`Extracting ${gccDownloadPath}`);
@@ -39,6 +50,9 @@ export async function install(release: string, platform?: string): Promise<strin
   } else {
     throw new Error(`Can't decompress ${distData.url}`);
   }
+
+  core.info(`Adding to cache: ${extractedPath}`);
+  await fs.promises.writeFile(path.join(extractedPath, 'md5.txt'), downloadHash, {encoding: 'utf8'});
   const cachedPath = await tc.cacheDir(extractedPath, toolName, release, platform);
   core.addPath(cachedPath);
 
