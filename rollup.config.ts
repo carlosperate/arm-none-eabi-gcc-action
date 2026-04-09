58bf5136fd1e83623fdb0c7662069e3d7695d098
Carlos Pereira Atencio <carlosperate@embeddedlog.com>
2026-04-09 13:11:26 +0200

    ci: Run `test-build` on all OSes to catch platform specific issues.

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,23 +1,23 @@
 // See: https://rollupjs.org/introduction/
 
-import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 
 const config = {
-  input: "src/main.ts",
+  input: 'src/main.ts',
   output: {
     esModule: true,
-    file: "dist/index.js",
-    format: "es",
+    file: 'dist/index.js',
+    format: 'es',
     sourcemap: true,
   },
   plugins: [
     typescript(),
-    nodeResolve({ preferBuiltins: true }),
-    commonjs({ transformMixedEsModules: true }),
-    json(),
+    nodeResolve({preferBuiltins: true}),
+    commonjs({transformMixedEsModules: true}),
+    json()
   ],
 };
 
